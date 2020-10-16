0996a93768c86b53e9f7082b5ad222d0a7844457
Rudinei Costa <costaaugusto177@outlook.com>
2020-10-16 18:02:32 -0300

    feat: created route orphanages with image

--- backend/src/infra/http/routes/orphanage.routes.ts
+++ backend/src/infra/http/routes/orphanage.routes.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
+import multer from 'multer';
+
 import OrphanageController from '../../../modules/orphanages/controller/OrphanageController';
+import UploadConfig from '../../../config/uploads';
 
 const orphanageRoute = Router();
+const uploads = multer(UploadConfig);
 
-orphanageRoute.post('/', OrphanageController.create);
+orphanageRoute.post('/', uploads.array('images'), OrphanageController.create);
 orphanageRoute.get('/', OrphanageController.index);
-orphanageRoute.get('/:id', OrphanageController.show);
+orphanageRoute.get('/:id',OrphanageController.show);
 
 export default orphanageRoute;
